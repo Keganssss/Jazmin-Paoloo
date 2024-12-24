39002897bf58665c0d6b6705459247c9347669a7
Keganssss <paollocardenas@gmail.com>
2024-12-24 17:34:52 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Este archivo está listo para incluir animaciones o funcionalidades adicionales
+// Puedes agregar más interactividad como un contador de poemas o efectos de desplazamiento
+
+window.onload = function() {
+    console.log("Página cargada con éxito");
+};
